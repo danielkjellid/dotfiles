051f30691d5af0d2d03de2b7db38a0c40a5a2064
Daniel Kjellid <42611331+danielkjellid@users.noreply.github.com>
2026-03-22 11:23:38 +0100

    Delete nvim tree

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,63 +1,59 @@
 -- Config is heavily inspired by MariaSolOs: https://github.com/MariaSolOs/dotfiles/blob/main/.config/nvim/
 
 -- Global variables
-vim.g.projects_dir = vim.env.HOME .. '/code'
-vim.g.work_projects_dir = vim.env.HOME .. '/volumes/git'
+vim.g.projects_dir = vim.env.HOME .. "/code"
+vim.g.work_projects_dir = vim.env.HOME .. "/volumes/git"
 vim.g.sidebar_width = 30 -- Width for nvim-tree and bufferline sidebar offset
 
 -- Install Lazy.
-local lazypath = vim.fn.stdpath 'data' .. '/lazy/lazy.nvim'
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not vim.uv.fs_stat(lazypath) then
-    vim.fn.system {
-        'git',
-        'clone',
-        '--filter=blob:none',
-        'https://github.com/folke/lazy.nvim.git',
-        '--branch=stable',
-        lazypath,
-    }
+	vim.fn.system({
+		"git",
+		"clone",
+		"--filter=blob:none",
+		"https://github.com/folke/lazy.nvim.git",
+		"--branch=stable",
+		lazypath,
+	})
 end
 vim.opt.rtp:prepend(lazypath)
 
 -- General setup
-require 'settings'
-require 'keymaps'
-require 'statusline'
-require 'winbar'
-require 'lsp'
+require("settings")
+require("keymaps")
+require("statusline")
+require("winbar")
+require("lsp")
 
 ---@type LazySpec
-local plugins = 'plugins'
-
+local plugins = "plugins"
 
 -- Configure plugins.
-require('lazy').setup(plugins, {
-  ui = { border = 'rounded' },
-  dev = { path = vim.g.projects_dir },
-  install = {
-    -- Do not automatically install on startup.
-    missing = true,
-  },
-  -- Don't bother me when tweaking plugins.
-  change_detection = { notify = false },
-  -- None of my plugins use luarocks so disable this.
-  rocks = {
-    enabled = false,
-  },
-  performance = {
-    rtp = {
-      -- Stuff I don't use.
-      disabled_plugins = {
-        'gzip',
-        'netrwPlugin',
-        'rplugin',
-        'tarPlugin',
-        'tohtml',
-        'zipPlugin',
-      },
-    },
-  },
+require("lazy").setup(plugins, {
+	ui = { border = "rounded" },
+	dev = { path = vim.g.projects_dir },
+	install = {
+		-- Do not automatically install on startup.
+		missing = true,
+	},
+	-- Don't bother me when tweaking plugins.
+	change_detection = { notify = false },
+	-- None of my plugins use luarocks so disable this.
+	rocks = {
+		enabled = false,
+	},
+	performance = {
+		rtp = {
+			-- Stuff I don't use.
+			disabled_plugins = {
+				"gzip",
+				"netrwPlugin",
+				"rplugin",
+				"tarPlugin",
+				"tohtml",
+				"zipPlugin",
+			},
+		},
+	},
 })
-
--- Interactive textual undotree:
--- vim.cmd.packadd 'nvim.undotree'
