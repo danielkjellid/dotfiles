67ca3b16fbc2ecd6751673ab209b3ee0d8e46991
Daniel Kjellid <42611331+danielkjellid@users.noreply.github.com>
2026-03-22 11:24:03 +0100

    Remove old config

--- nvim/old/lua/config/colorscheme.lua
+++
@@ -1,7 +0,0 @@
-local colorscheme = "catppuccin"
-
-local status_ok, _scheme = pcall(vim.cmd, "colorscheme " .. colorscheme)
-if not status_ok then
-  vim.notify("colorscheme " .. colorscheme .. " not found! Falling back to default")
-  return
-end
--- nvim/old/lua/config/venv-lsp.lua
+++
@@ -1,8 +0,0 @@
-local status_ok, pckg = pcall(require, "venv-lsp")
-
-if not status_ok then
-  vim.notify("Unable to require venv-lsp, skipping")
-  return
-end
-
-return pckg.setup()
